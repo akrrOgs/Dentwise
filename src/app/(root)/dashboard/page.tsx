e93df3b256b32fc028812341d289641c08443a9a
akrrOgs <dlordrog@gmail.com>
2025-10-17 20:22:54 +0800

    Implement Dashboard Pages

--- src/app/(root)/dashboard/page.tsx
+++ src/app/(root)/dashboard/page.tsx
@@ -1,13 +1,20 @@
-import Navbar from "@/components/Navbar"
+import ActivityOverview from "@/components/dashboard/ActivityOverview";
+import MainActions from "@/components/dashboard/MainAction";
+import WelcomeSection from "@/components/dashboard/WelcomeSection";
+import Navbar from "@/components/Navbar";
 
 const Dashboard = () => {
   return (
-    <div>
+    <>
       <Navbar />
 
-      Dashboard
-    </div>
-  )
-}
+      <div className="max-w-7xl mx-auto px-6 py-8 pt-24">
+        <WelcomeSection />
+        <MainActions />
+        <ActivityOverview />
+      </div>
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
