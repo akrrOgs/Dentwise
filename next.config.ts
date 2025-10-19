35f2b6af8915763d846dbf0139c029ad1b8c6bf6
akrrOgs <137786263+akrrOgs@users.noreply.github.com>
2025-10-19 20:17:15 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,12 @@ const nextConfig: NextConfig = {
     ],
     unoptimized: true,
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
